3f7b4104129aecf7937ff6b40e1c320974492bc4
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2020-01-28 20:24:25 +0200

    added some code to moi.js and hei.js

--- hei.js
+++ hei.js
@@ -1,1 +1,9 @@
-<p>Hei Ilpo</p>
+<html>
+<head>
+    <title>Hei Ilpo</title>
+</head>
+<body>
+    <h1>Otsikko</h1>
+    <p>Hei Ilpo</p>
+</body>
+</html>
--- moi.js
+++ moi.js
@@ -1,1 +1,12 @@
+<html>
+<body>
 <p>Moi Ilpo</p>
+
+<script>
+    function multiply(x, y) {
+        x * y = z
+        return z
+    }
+</script>
+</body>
+</html>
