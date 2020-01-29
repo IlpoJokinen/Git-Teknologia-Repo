a8bc805d8dcedd9a5b21c67ad134e39bb2139ded
Ilpo Jokinen <bgg300@myy.haaga-helia.fi>
2020-01-29 20:30:14 +0200

    muutoksia dev.js tiedostoon kloonatun etärepositoryn master haarassa

--- dev.js
+++ dev.js
@@ -1,15 +1,21 @@
 const randomThing = () => {
 
-function loop(x) {
-    for (var i = 0; i < x.length; i++) {
-        return <p>something</p>
+    function uusiFunction {
+        return null
     }
-}
-function divide(x, y) {
-    return x / y
-}
+    
+    function loop(x) {
+        for (var i = 0; i < x.length; i++) {
+            return <p>something</p>
+        }
+    }
+    function divide(x, y) {
+        return x / y
+    }
 
-return divide
-
+    return (
+        <b>Tämä on muutos etärepositoryn master haarassa!</b>
+        divide
+    )
 }
 
